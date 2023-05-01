121e575aa56c5b068e28a07d0b400fc01bf6708f
AthithyanKannan <athithyan1314@gmail.com>
2023-05-01 12:56:00 +0530

    After Reset,First Commit

--- lib/pages/update_screens/get_events.dart
+++ lib/pages/update_screens/get_events.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-String DeletedocumentId = 'Athithyan';
+String DeletedocumentId = '';
 
 setEventDocumentId(String id) {
   DeletedocumentId = id;
